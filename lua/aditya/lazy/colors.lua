87f84f30a60ff3e019b87943f00b3dcd2137353f
AdityaC4 <adityapchaudhari@gmail.com>
2024-12-12 14:47:06 -0500

    more colors

--- lua/aditya/lazy/colors.lua
+++ lua/aditya/lazy/colors.lua
@@ -6,7 +6,18 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
+local colorschemes = { "rose-pine", "gruvbox", "tokyonight" }
+local index = 1
+
+vim.keymap.set("n", "<leader>cc", function()
+	index = (index % #colorschemes) + 1
+	local scheme = colorschemes[index]
+	vim.notify("Switching to colorscheme: " .. scheme)
+	ColorMyPencils(scheme)
+end, { desc = "Cycle Colorschemes" })
+
 return {
+	-- Rose Pine
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
@@ -18,6 +29,28 @@ return {
 				},
 			})
 			ColorMyPencils("rose-pine")
+		end,
+	},
+	-- Gruvbox colorscheme
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard", -- Options: 'hard', 'soft', 'medium'
+				transparent_mode = true,
+			})
+		end,
+	},
+	-- Tokyo Night colorscheme
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		config = function()
+			require("tokyonight").setup({
+				style = "moon", -- Options: 'storm', 'night', 'day', 'moon'
+				transparent = true,
+			})
 		end,
 	},
 }
