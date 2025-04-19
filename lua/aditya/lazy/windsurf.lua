35c17c2b785e73132405b4ef602842a56c922cc1
AdityaC4 <adityapchaudhari@gmail.com>
2025-04-19 01:26:46 -0400

    windsurf commented

---
+++ lua/aditya/lazy/windsurf.lua
@@ -0,0 +1,24 @@
+-- return {
+-- 	{
+-- 		"Exafunction/windsurf.vim",
+-- 		event = "InsertEnter",
+-- 		config = function()
+-- 			-- Optional: Disable default keybindings
+-- 			vim.g.codeium_disable_bindings = 1
+--
+-- 			-- Custom keybindings
+-- 			vim.keymap.set("i", "<C-g>", function()
+-- 				return vim.fn["codeium#Accept"]()
+-- 			end, { expr = true, silent = true })
+-- 			vim.keymap.set("i", "<C-;>", function()
+-- 				return vim.fn["codeium#CycleCompletions"](1)
+-- 			end, { expr = true, silent = true })
+-- 			vim.keymap.set("i", "<C-,>", function()
+-- 				return vim.fn["codeium#CycleCompletions"](-1)
+-- 			end, { expr = true, silent = true })
+-- 			vim.keymap.set("i", "<C-x>", function()
+-- 				return vim.fn["codeium#Clear"]()
+-- 			end, { expr = true, silent = true })
+-- 		end,
+-- 	},
+-- }
